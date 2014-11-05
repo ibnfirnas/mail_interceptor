bc98fb9da9a5341ab4d493bac2ed6c7cc1f98188
Siraaj Khandkar <siraaj@khandkar.net>
2014-11-05 16:43:08 -0500

    Simplify digest function.

--- src/lib/mail_db/mail_db.ml
+++ src/lib/mail_db/mail_db.ml
@@ -1,21 +1,6 @@
 open Core.Std
 open Async.Std
 
-module Digest : sig
-  type t = string
-
-  val of_string : string -> t
-end = struct
-  type t = string
-
-  let of_string message =
-    let hash = Cryptokit.Hash.sha3 256 in
-    let hex = Cryptokit.Hexa.encode () in
-    hash#add_string message;
-    hex#put_string hash#result;
-    hex#get_string
-end
-
 type t =
   { dir_messages  : string
   ; dir_mailboxes : string
@@ -26,6 +11,11 @@ let ( / ) = Filename.concat
 let ensure_directories {dir_messages; dir_mailboxes} =
   Async_shell.mkdir ~p:() dir_messages >>= fun () ->
   Async_shell.mkdir ~p:() dir_mailboxes
+
+let digest_of_string s =
+  let hash      = Cryptokit.Hash.sha3 256 in
+  let transform = Cryptokit.Hexa.encode () in
+  Cryptokit.transform_string transform (Cryptokit.hash_string hash s)
 
 let init ~directory:root =
   let subdir_messages  = "messages" in
@@ -38,7 +28,7 @@ let init ~directory:root =
   return t
 
 let store ({dir_messages; dir_mailboxes} as t) ~receiver ~msg =
-  let msg_digest = Digest.of_string msg in
+  let msg_digest = digest_of_string msg in
   let path_to_msg      = dir_messages  / msg_digest in
   let path_to_manifest = dir_mailboxes / receiver in
   ensure_directories t
