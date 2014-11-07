60d918e089cafc8e3b08b8d72374b5f82abd46a7
Siraaj Khandkar <siraaj@khandkar.net>
2014-11-07 00:15:54 -0500

    Move worker definitions out of main.

--- src/bin/mail_interceptor.ml
+++ src/bin/mail_interceptor.ml
@@ -22,6 +22,36 @@ end = struct
       workers
 end
 
+let worker_storer ~smtp_msgs_r ~db () =
+  let store_smtp_msg (time, (_sender, receivers, _email_id, email_msg)) =
+    let msg = Mail_db_msg.cons ~time ~email_msg in
+    Deferred.List.iter receivers ~how:`Parallel ~f:(fun receiver ->
+      Log.Global.debug "Storing messsage for receiver: %s" receiver;
+      let receiver = receiver
+        |> String.lstrip ~drop:((=) '<')
+        |> String.rstrip ~drop:((=) '>')
+      in
+      Mail_db.store db ~receiver ~msg
+    )
+  in
+  Pipe.iter smtp_msgs_r ~f:store_smtp_msg
+
+let worker_server ~smtp_msgs_w ~port () =
+  let router ~addr ~r ~w =
+    let routing_rule smtp_msg =
+      Pipe.write_without_pushback smtp_msgs_w (Time.now (), smtp_msg);
+      None
+    in
+    Smtp.Router.rules_server [] [routing_rule] addr r w
+  in
+  ( Tcp.Server.create
+      ~on_handler_error:`Raise
+      (Tcp.on_port port)
+      (fun addr r w -> router ~addr ~r ~w)
+  )
+  >>= fun _address ->
+  Deferred.never ()
+
 let main ~directory ~port ~log_level ~release_parent =
   let log_dir_path  = directory    ^/ "log" in
   let log_file_path = log_dir_path ^/ "console.log" in
@@ -35,40 +65,10 @@ let main ~directory ~port ~log_level ~release_parent =
   Mail_db.init ~directory
   >>= fun db ->
   let smtp_msgs_r, smtp_msgs_w = Pipe.create () in
-  let worker_storer () =
-    let store_smtp_msg (time, (_sender, receivers, _email_id, email_msg)) =
-      let msg = Mail_db_msg.cons ~time ~email_msg in
-      Deferred.List.iter receivers ~how:`Parallel ~f:(fun receiver ->
-        Log.Global.debug "Storing messsage for receiver: %s" receiver;
-        let receiver = receiver
-          |> String.lstrip ~drop:((=) '<')
-          |> String.rstrip ~drop:((=) '>')
-        in
-        Mail_db.store db ~receiver ~msg
-      )
-    in
-    Pipe.iter smtp_msgs_r ~f:store_smtp_msg
-  in
-  let worker_server () =
-    let router ~addr ~r ~w =
-      let routing_rule smtp_msg =
-        Pipe.write_without_pushback smtp_msgs_w (Time.now (), smtp_msg);
-        None
-      in
-      Smtp.Router.rules_server [] [routing_rule] addr r w
-    in
-    ( Tcp.Server.create
-        ~on_handler_error:`Raise
-        (Tcp.on_port port)
-        (fun addr r w -> router ~addr ~r ~w)
-    )
-    >>= fun _address ->
-    Deferred.never ()
-  in
   release_parent ();
   Supervisor.run
-    [ worker_storer
-    ; worker_server
+    [ worker_storer ~smtp_msgs_r ~db
+    ; worker_server ~smtp_msgs_w ~port
     ]
 
 let () =
