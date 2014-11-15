7fde71ff2c45b6d45cebb48f3164bae7912d367a
Siraaj Khandkar <siraaj@khandkar.net>
2014-11-14 20:33:54 -0500

    Organize CLI into subcommands.

--- src/bin/mail_interceptor.ml
+++ src/bin/mail_interceptor.ml
@@ -1,33 +1,70 @@
 open Core.Std
 open Async.Std
 
-let () =
-  let (+) = Command.Spec.(+>) in
-  Command.run (Command.async_basic
-    ~summary:""
-    Command.Spec.
-    ( empty
-    + flag "--storage-directory" (required string)
-        ~doc:" Where to store intercepted messages?"
-    + flag "--port" (optional_with_default 2525 int)
-        ~doc:" TCP port to listen on. Default: 2525"
-    + flag "--log-level" (optional_with_default "Info" string)
-        ~doc:" Log level [Debug | Info | Error]. Default: Info"
-    + flag "--daemonize" no_arg
-        ~doc:" Shall we daemonize? Default: no"
+module Flag = struct
+  open Command.Spec
+
+  module Default = struct
+    let port      = 2525
+    let log_level = "Info"
+  end
+
+  let directory =
+    flag "--storage-directory" (required string)
+    ~doc:" Where to store intercepted messages?"
+
+  let port =
+    flag "--port" (optional_with_default Default.port int)
+    ~doc:(sprintf " TCP port to listen on. Default: %d" Default.port)
+
+  let log_level =
+    flag "--log-level" (optional_with_default Default.log_level string)
+    ~doc:(sprintf " Log level [Debug | Info | Error]. Default: %s" Default.log_level)
+end
+
+module Spec = struct
+  let for_start =
+    let (+) = Command.Spec.(+>) in
+    ( Command.Spec.empty
+    + Flag.directory
+    + Flag.port
+    + Flag.log_level
     )
-    ( fun directory port log_level daemonize () ->
-        let log_level = Log.Level.of_string log_level in
+end
+
+let command_start_foreground =
+  Command.async_basic
+    ~summary:"Start server in the foreground."
+    Spec.for_start
+    ( fun directory port log_level () ->
+        Mail_interceptor_server.start
+          ~directory
+          ~port
+          ~log_level:(Log.Level.of_string log_level)
+          ~release_parent:(fun () -> ())
+    )
+
+let command_start_daemon =
+  Command.async_basic
+    ~summary:"Start server in the background (daemonize)."
+    Spec.for_start
+    ( fun directory port log_level () ->
         let release_parent =
-          if daemonize then
-            Staged.unstage (Daemon.daemonize_wait ~cd:directory ())
-          else
-            fun () -> ()
+          Staged.unstage (Daemon.daemonize_wait ~cd:directory ())
         in
         Mail_interceptor_server.start
           ~directory
           ~port
-          ~log_level
+          ~log_level:(Log.Level.of_string log_level)
           ~release_parent
     )
-  )
+
+let command =
+  Command.group
+    ~summary:""
+    [ "run"   , command_start_foreground
+    ; "start" , command_start_daemon
+    ]
+
+let () =
+  Command.run command
